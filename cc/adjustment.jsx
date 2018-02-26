3bbb879593704fad9e9922ee3e8522fb95d15659
Petri Damstén <petri.damsten@gmail.com>
2018-02-26 21:55:51 +0200

    channelMixer, SolidColor, HueSaturation, SelectiveColor

--- cc/adjustment.jsx
+++ cc/adjustment.jsx
@@ -57,9 +57,193 @@ setCurveAdjustment = function(layer, data)
     desc2.putList(cTID('Adjs'), list1);
     desc1.putObject(cTID('T   '), cTID('Crvs'), desc2);
     executeAction(cTID('setd'), desc1, DialogModes.NO);
+    return true;
   } catch (err) {
     log('setCurveAdjustment', e.message);
      return false;
   }
-  return true;
+}
+
+createChannelMixer = function(name, layer)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(cTID('AdjL'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    var desc3 = new ActionDescriptor();
+    desc3.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindDefault"));
+    var desc4 = new ActionDescriptor();
+    desc4.putUnitDouble(cTID('Rd  '), cTID('#Prc'), 100);
+    desc3.putObject(cTID('Rd  '), cTID('ChMx'), desc4);
+    var desc5 = new ActionDescriptor();
+    desc5.putUnitDouble(cTID('Grn '), cTID('#Prc'), 100);
+    desc3.putObject(cTID('Grn '), cTID('ChMx'), desc5);
+    var desc6 = new ActionDescriptor();
+    desc6.putUnitDouble(cTID('Bl  '), cTID('#Prc'), 100);
+    desc3.putObject(cTID('Bl  '), cTID('ChMx'), desc6);
+    desc2.putObject(cTID('Type'), cTID('ChnM'), desc3);
+    desc1.putObject(cTID('Usng'), cTID('AdjL'), desc2);
+    executeAction(cTID('Mk  '), desc1, DialogModes.NO);
+    app.activeDocument.activeLayer.name = name;
+    return app.activeDocument.activeLayer;
+  } catch (err) {
+    log('createChannelMixer', e.message);
+    return null;
+  }
+}
+
+setChannelMixer = function(layer, color, cnst, mono)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putEnumerated(cTID('AdjL'), cTID('Ordn'), cTID('Trgt'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    desc2.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindCustom"));
+    desc2.putBoolean(cTID('Mnch'), mono);
+    var desc3 = new ActionDescriptor();
+    desc3.putUnitDouble(cTID('Rd  '), cTID('#Prc'), color[0]);
+    desc3.putUnitDouble(cTID('Grn '), cTID('#Prc'), color[1]);
+    desc3.putUnitDouble(cTID('Bl  '), cTID('#Prc'), color[2]);
+    desc3.putUnitDouble(cTID('Cnst'), cTID('#Prc'), cnst);
+    desc2.putObject(cTID('Gry '), cTID('ChMx'), desc3);
+    desc1.putObject(cTID('T   '), cTID('ChnM'), desc2);
+    executeAction(cTID('setd'), desc1, DialogModes.NO);
+    return true;
+  } catch (err) {
+    log('setChannelMixer', e.message);
+     return false;
+  }
+}
+
+createSolidColorAdjustment = function(name, layer, color)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(sTID("contentLayer"));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    var desc3 = new ActionDescriptor();
+    var desc4 = new ActionDescriptor();
+    desc4.putDouble(cTID('Rd  '), color[0]);
+    desc4.putDouble(cTID('Grn '), color[1]);
+    desc4.putDouble(cTID('Bl  '), color[2]);
+    desc3.putObject(cTID('Clr '), sTID("RGBColor"), desc4);
+    desc2.putObject(cTID('Type'), sTID("solidColorLayer"), desc3);
+    desc1.putObject(cTID('Usng'), sTID("contentLayer"), desc2);
+    executeAction(cTID('Mk  '), desc1, DialogModes.NO);
+    app.activeDocument.activeLayer.name = name;
+    return app.activeDocument.activeLayer;
+  } catch (err) {
+    log('createSolidColorAdjustment', e.message);
+     return null;
+  }
+};
+
+createHueSaturationAdjustment = function(name, layer)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(cTID('AdjL'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    var desc3 = new ActionDescriptor();
+    desc3.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindDefault"));
+    desc3.putBoolean(cTID('Clrz'), false);
+    desc2.putObject(cTID('Type'), cTID('HStr'), desc3);
+    desc1.putObject(cTID('Usng'), cTID('AdjL'), desc2);
+    executeAction(cTID('Mk  '), desc1, DialogModes.NO);
+    app.activeDocument.activeLayer.name = name;
+    return app.activeDocument.activeLayer;
+  } catch (err) {
+    log('createHueSaturationAdjustment', e.message);
+    return null;
+  }
+}
+
+setHueSaturationAdjustment = function(layer, hue, saturation, lightness)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putEnumerated(cTID('AdjL'), cTID('Ordn'), cTID('Trgt'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    desc2.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindCustom"));
+    var list1 = new ActionList();
+    var desc3 = new ActionDescriptor();
+    desc3.putInteger(cTID('H   '), hue);
+    desc3.putInteger(cTID('Strt'), saturation);
+    desc3.putInteger(cTID('Lght'), lightness);
+    list1.putObject(cTID('Hst2'), desc3);
+    desc2.putList(cTID('Adjs'), list1);
+    desc1.putObject(cTID('T   '), cTID('HStr'), desc2);
+    executeAction(cTID('setd'), desc1, DialogModes.NO);
+    return true;
+  } catch (err) {
+    log('setHueSaturationAdjustment', e.message);
+    return false;
+  }
+}
+
+createSelectiveColorAdjustment = function(name, layer)
+{
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(cTID('AdjL'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    var desc3 = new ActionDescriptor();
+    desc3.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindDefault"));
+    desc2.putObject(cTID('Type'), cTID('SlcC'), desc3);
+    desc1.putObject(cTID('Usng'), cTID('AdjL'), desc2);
+    executeAction(cTID('Mk  '), desc1, DialogModes.NO);
+    app.activeDocument.activeLayer.name = name;
+    return app.activeDocument.activeLayer;
+  } catch (err) {
+    log('createSelectiveColorAdjustment', e.message);
+    return null;
+  }
+}
+
+setSelectiveColorAdjustment = function(layer, color, values)
+{
+  var colors = {'reds': 'Rds ', 'yellows': 'Ylws', 'greens': 'Grns', 'cyans': 'Cyns',
+                'blues': 'Bls ', 'magentas': 'Mgnt', 'whites': 'Whts', 'neutrals': 'Ntrl',
+                'blacks': 'Blks'}
+  try {
+    activateLayer(layer);
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putEnumerated(cTID('AdjL'), cTID('Ordn'), cTID('Trgt'));
+    desc1.putReference(cTID('null'), ref1);
+    var desc2 = new ActionDescriptor();
+    var list1 = new ActionList();
+    var desc3 = new ActionDescriptor();
+    desc3.putEnumerated(cTID('Clrs'), cTID('Clrs'), cTID(colors[color]));
+    desc3.putUnitDouble(cTID('Cyn '), cTID('#Prc'), values[0]);
+    desc3.putUnitDouble(cTID('Mgnt'), cTID('#Prc'), values[1]);
+    desc3.putUnitDouble(cTID('Ylw '), cTID('#Prc'), values[2]);
+    desc3.putUnitDouble(cTID('Blck'), cTID('#Prc'), values[3]);
+    list1.putObject(cTID('ClrC'), desc3);
+    desc2.putList(cTID('ClrC'), list1);
+    desc1.putObject(cTID('T   '), cTID('SlcC'), desc2);
+    executeAction(cTID('setd'), desc1, DialogModes.NO);
+    return true;
+  } catch (err) {
+    log('setSelectiveColorAdjustment', e.message);
+    return false;
+  }
 }
