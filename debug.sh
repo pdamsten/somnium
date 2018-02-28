6c2dce4d738eb3c07e2d49e518023605d3edccbb
Petri Damstén <petri.damsten@gmail.com>
2018-02-28 15:48:52 +0200

    Add .debug creation

--- debug.sh
+++ debug.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+cat > .debug <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<ExtensionList>
+    <Extension Id="com.petridamsten.somnium.panel">
+        <HostList>
+            <!-- Photoshop -->
+            <Host Name="PHXS" Port="8088"/>
+        </HostList>
+    </Extension>
+</ExtensionList>
+EOL
+
 defaults write com.adobe.CSXS.8 PlayerDebugMode 1
 defaults write com.adobe.CSXS.8 LogLevel 6 && killall -u `whoami` cfprefsd
