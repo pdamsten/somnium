cb7fd6d002d9a28ddf4cafbdbe39a33d07e323fd
Petri Damstén <petri.damsten@gmail.com>
2018-03-01 19:52:21 +0200

    make package in separate dir so only needed is included.

--- make.sh
+++ make.sh
@@ -6,9 +6,29 @@ if [ ! -f ~/.pspassword ]; then
 fi
 PASSWORD=$(cat ~/.pspassword)
 
-rm log.txt
+mkdir ../tmp &>/dev/null
+cd ../tmp
+rm -fR com.petridamsten.somnium &>/dev/null
 
-cd ..
+mkdir com.petridamsten.somnium
+mkdir com.petridamsten.somnium/cc
+cp ../com.petridamsten.somnium/cc/*.jsx com.petridamsten.somnium/cc/
+mkdir com.petridamsten.somnium/CSXS
+cp ../com.petridamsten.somnium/CSXS/*.xml com.petridamsten.somnium/CSXS/
+mkdir com.petridamsten.somnium/ui
+cp ../com.petridamsten.somnium/ui/*.html com.petridamsten.somnium/ui/
+mkdir com.petridamsten.somnium/ui/css
+cp ../com.petridamsten.somnium/ui/css/*.css com.petridamsten.somnium/ui/css/
+mkdir com.petridamsten.somnium/ui/img
+cp ../com.petridamsten.somnium/ui/img/*.png com.petridamsten.somnium/ui/img/
+cp ../com.petridamsten.somnium/ui/img/ic_*.svg com.petridamsten.somnium/ui/img/
+cp ../com.petridamsten.somnium/ui/img/*logo.svg com.petridamsten.somnium/ui/img/
+mkdir com.petridamsten.somnium/ui/js
+cp ../com.petridamsten.somnium/ui/js/*.js com.petridamsten.somnium/ui/js/
+mkdir com.petridamsten.somnium/ui/js/libs
+cp ../com.petridamsten.somnium/ui/js/libs/*.js com.petridamsten.somnium/ui/js/libs/
+
+rm com.petridamsten.somnium.zxp &>/dev/null
 
 if [ ! -f selfDB.p12 ]; then
   ZXPSignCmd -selfSignedCert FI PS PetriDamsten "Petri Damstén" "$PASSWORD" selfDB.p12
