c5d1862e91b9196b7685f884d8f4599a3e4189ab
Petri Damstén <petri.damsten@gmail.com>
2018-03-13 09:53:36 +0200

    write files to a dir. Add ffmpeg scripts.

--- cc/main.jsx
+++ cc/main.jsx
@@ -166,6 +166,15 @@ function onDeleteAllClick()
 //
 //**************************************************************************
 
+var pindex = 0;
+var mogrify = '';
+var ffmpeg = '';
+var ffmpeglast = '';
+
+var minLength = 3.5 // seconds
+var maxLength = 10.0 // seconds
+var defaultFps = 3.0
+
 onSaveLayersClick = function()
 {
   try {
@@ -186,7 +195,7 @@ saveLayersAsJpgs = function(layers)
 saveJpg = function(n, layer)
 {
   layer.visible = true;
-  saveAsJpeg(output('p', n, '.jpg'), 3840, 2160);
+  saveAsJpeg(output('layer-', n, '.jpg'), 3840, 2160);
 }
 
 hideAllLayers = function(layers)
@@ -202,19 +211,32 @@ hideAllLayers = function(layers)
 
 output = function(prefix, n, suffix)
 {
-  var parts = app.activeDocument.name.split(".");
-  var fname = parts[0];
-  return '~/tmp/' + prefix + '-' + fname + '-' + padNum(n + 1, 4) + suffix;
+  var dir = checkDir();
+  var name = dir + prefix + padNum(n + 1, 4) + suffix;
+  ffmpeglast = 'file ' + name + '\n';
+  ffmpeg += ffmpeglast;
+  mogrify += name + '\n';
+  return name;
 }
 
-var pindex = 0;
+checkDir = function()
+{
+  var parts = app.activeDocument.name.split(".");
+  var fname = parts[0];
+  var s = File('~/tmp/' + fname).fsName + '/';
+  mkdir(s);
+  return s;
+}
 
 handleLayers = function(layers, func)
 {
+  pindex = 0;
+  mogrify = '';
+  ffmpeg = '';
+  ffmpeglast = '';
+
   var lmain = findMain(layers);
   var masks = false;
-
-  pindex = 0;
 
   if (lmain != -1) {
     handleMain(lmain, layers, func);
@@ -241,6 +263,32 @@ handleLayers = function(layers, func)
       func.apply(this, [pindex++, layer]);
     }
   }
+
+  var d = checkDir();
+  writeTextFile(d + 'convert.txt', mogrify);
+  writeTextFile(d + 'ffmpeg.txt', ffmpeg + ffmpeglast);
+
+  var parts = app.activeDocument.name.split(".");
+  var fname = parts[0];
+  var fr = 0;
+  var n = pindex - 1;
+  var insta = '-vf "scale=1080:1080:force_original_aspect_ratio=decrease,pad=1080:1080:(ow-iw)/2:(oh-ih)/2" ';
+  var fullhd = '-vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2" ';
+  var fourk = '-vf "scale=3840:2160:force_original_aspect_ratio=decrease,pad=3840:2160:(ow-iw)/2:(oh-ih)/2" ';
+
+  if (n / defaultFps < minLength) {
+      fr = n / minLength;
+  } else if (n / defaultFps > maxLength) {
+      fr = n / maxLength;
+  } else {
+      fr = defaultFps;
+  }
+  cmd1 = '#!/bin/bash\nffmpeg -f concat -safe 0 -r ' +  fr + ' -i "' + d + 'ffmpeg.txt" ';
+  cmd2 = '-c:v libx264 -r 30 -pix_fmt yuv420p "' + d + fname;
+  writeTextFile(d + 'ffmpeg-org.sh', cmd1 + cmd2 + '.mp4"\n');
+  writeTextFile(d + 'ffmpeg-4k.sh', cmd1 + fourk + cmd2 + '-4k.mp4"\n');
+  writeTextFile(d + 'ffmpeg-1080p.sh', cmd1 + fullhd + cmd2 + '-1080p.mp4"\n');
+  writeTextFile(d + 'ffmpeg-insta.sh', cmd1 + insta + cmd2 + '-insta.mp4"\n');
 }
 
 findMain = function(layers)
@@ -302,7 +350,7 @@ takeScreenshot = function(n, layer)
   layer.visible = true;
   app.activeDocument.activeLayer = layer;
   app.refresh();
-  app.system("/usr/sbin/screencapture -x -m -T 0 " + output('s', n, '.png'));
+  app.system("/usr/sbin/screencapture -x -m -T 0 " + output('screenshot-', n, '.png'));
 }
 
 takeScreenshotsFromLayers = function(layers)
