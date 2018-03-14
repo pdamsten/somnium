227f173032474327d2fc44ec59bf0c423b302b67
Petri Damstén <petri.damsten@gmail.com>
2018-03-14 17:48:14 +0200

    doApplyImage

--- cc/adjustment.jsx
+++ cc/adjustment.jsx
@@ -6,9 +6,6 @@
 //
 //**************************************************************************
 
-cTID = function(s) { return app.charIDToTypeID(s); };
-sTID = function(s) { return app.stringIDToTypeID(s); };
-
 doGaussianBlur = function(layer, value, withDialog)
 {
   try {
@@ -59,6 +56,27 @@ doHighPass = function(layer, value, withDialog)
     var desc1 = new ActionDescriptor();
     desc1.putUnitDouble(cTID('Rds '), cTID('#Pxl'), value);
     executeAction(sTID('highPass'), desc1, dialogMode);
+  } catch (e) {
+    log(e);
+  }
+}
+
+doApplyImage = function(layer, values, withDialog)
+{
+  try {
+    activateLayer(layer);
+    var dialogMode = (typeof withDialog === 'undefined' || withDialog) ? DialogModes.ALL : DialogModes.NO;
+    var desc1 = new ActionDescriptor();
+    var desc2 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putEnumerated(cTID('Chnl'), cTID('Chnl'), sTID(values[0]));
+    ref1.putName(cTID('Lyr '), values[1]);
+    desc2.putReference(cTID('T   '), ref1);
+    desc2.putEnumerated(cTID('Clcl'), cTID('Clcn'), blendingMode(values[2]));
+    desc2.putDouble(cTID('Scl '), values[3]);
+    desc2.putInteger(cTID('Ofst'), values[4]);
+    desc1.putObject(cTID('With'), cTID('Clcl'), desc2);
+    executeAction(sTID('applyImageEvent'), desc1, dialogMode);
   } catch (e) {
     log(e);
   }
