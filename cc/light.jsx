6b904741b7e6add5679089288f4f393562ad3767
Petri Damstén <petri.damsten@gmail.com>
2018-03-21 09:32:56 +0200

    Add place for finish light code

---
+++ cc/light.jsx
@@ -0,0 +1,38 @@
+//**************************************************************************
+//
+//   Copyright (c) 2018 by Petri Damstén <petri.damsten@gmail.com>
+//
+//   Finish - Light tab code
+//
+//**************************************************************************
+
+
+onMakeCookie = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
+
+onMakeVignetteClick = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
+
+onWashOutClick = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
+
+onMakeDBCurves = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
+
+onMakeDBGray = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
+
+onHighlight = function()
+{
+  var group = checkGroup('Light', 'Finish', 'Help Layers')
+}
