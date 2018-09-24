7d6904d32cf2a6d9cf29172a069a8f914498c032
Petri Damstén <petri.damsten@gmail.com>
2018-09-24 09:35:58 +0300

    read options from settings. add try catch to all.

--- cc/light.jsx
+++ cc/light.jsx
@@ -6,10 +6,11 @@
 //
 //**************************************************************************
 
-onMakeCookieClick = function(type)
+onMakeCookieClick = function()
 {
+  var type = settings.value('MakeCookie', 'type');
   var group = checkGroup('Light', 'Finish', 'Help Layers');
-  if (type == 'darken') {
+  if (type == 0) {
     var c = [curvePoint(0, 0), curvePoint(128, -15), curvePoint(255, 0)];
     l = createCurveAdjustment('Cookie', group);
     setCurveAdjustment(l, c);
@@ -22,9 +23,10 @@ onMakeCookieClick = function(type)
   }
 }
 
-onMakeVignetteClick = function(type)
+onMakeVignetteClick = function()
 {
   try {
+    var type = settings.value('MakeVignette', 'type');
     var group = checkGroup('Light', 'Finish', 'Help Layers');
     var p = 0.1;
     var c = [app.activeDocument.height * p, app.activeDocument.width * p,
@@ -44,47 +46,60 @@ onMakeVignetteClick = function(type)
 
 onWashOutClick = function()
 {
-  var group = checkGroup('Light', 'Finish', 'Help Layers');
-  var c = [curvePoint(0, 5), curvePoint(32, 0), curvePoint(224, 0), curvePoint(255, -2.5)];
+  try {
+    var group = checkGroup('Light', 'Finish', 'Help Layers');
+    var c = [curvePoint(0, 5), curvePoint(32, 0), curvePoint(224, 0), curvePoint(255, -2.5)];
 
-  l = createCurveAdjustment('Wash Out', group);
-  setCurveAdjustment(l, c);
-  deleteLayerMask(l);
-  setLayerBlendingMode(l, 'luminosity');
+    l = createCurveAdjustment('Wash Out', group);
+    setCurveAdjustment(l, c);
+    deleteLayerMask(l);
+    setLayerBlendingMode(l, 'luminosity');
+  } catch (e) {
+    log(e);
+  }
 }
 
-onMakeDBClick = function(type)
+onMakeDBClick = function()
 {
-  var group = checkGroup('Light', 'Finish', 'Help Layers');
+  try {
+    var type = settings.value('MakeDB', 'type');
+    var group = checkGroup('Light', 'Finish', 'Help Layers');
 
-  if (type == 'grey') {
-    var l = createLayer('Dodge & Burn', group);
-    fillLayer(l, 'gray');
-    setLayerBlendingMode(l, 'overlay');
-    selectTool('burn');
-  } else {
-    group = createGroup('Dodge & Burn', group);
-    var b = [curvePoint(0, 0), curvePoint(128, -30), curvePoint(255, 0)];
-    var d = [curvePoint(0, 0), curvePoint(128, 30), curvePoint(255, 0)];
-    var dl = createCurveAdjustment('Dodge', group);
-    var bl = createCurveAdjustment('Burn', group);
-    setCurveAdjustment(dl, d);
-    setCurveAdjustment(bl, b);
-    invertLayerMask(dl);
-    invertLayerMask(bl);
-    setLayerBlendingMode(dl, 'luminosity');
-    setLayerBlendingMode(bl, 'luminosity');
-    setDefaultColors();
-    selectTool('brush'); // TODO brush preset?
+    if (type == 1) {
+      var l = createLayer('Dodge & Burn', group);
+      fillLayer(l, 'gray');
+      setLayerBlendingMode(l, 'overlay');
+      selectTool('burn');
+    } else {
+      group = createGroup('Dodge & Burn', group);
+      var b = [curvePoint(0, 0), curvePoint(128, -30), curvePoint(255, 0)];
+      var d = [curvePoint(0, 0), curvePoint(128, 30), curvePoint(255, 0)];
+      var dl = createCurveAdjustment('Dodge', group);
+      var bl = createCurveAdjustment('Burn', group);
+      setCurveAdjustment(dl, d);
+      setCurveAdjustment(bl, b);
+      invertLayerMask(dl);
+      invertLayerMask(bl);
+      setLayerBlendingMode(dl, 'luminosity');
+      setLayerBlendingMode(bl, 'luminosity');
+      setDefaultColors();
+      selectTool('brush'); // TODO brush preset?
+    }
+  } catch (e) {
+    log(e);
   }
 }
 
 onHighlightClick = function()
 {
-  var group = checkGroup('Light', 'Finish', 'Help Layers');
+  try {
+    var group = checkGroup('Light', 'Finish', 'Help Layers');
 
-  var l = createSolidColorAdjustment('Hightlight', group, [255, 255, 255])
-  setLayerBlendingMode(l, 'softLight');
-  addLayerMask(l);
-  invertLayerMask(l);
+    var l = createSolidColorAdjustment('Hightlight', group, [255, 255, 255])
+    setLayerBlendingMode(l, 'softLight');
+    addLayerMask(l);
+    invertLayerMask(l);
+  } catch (e) {
+    log(e);
+  }
 }
