c005c8a4e3b65e73d9c76ff9b9222f7c31bcb14e
Petri Damstén <petri.damsten@gmail.com>
2018-09-26 10:01:08 +0300

    Upper case headers

--- fill_actions.py
+++ fill_actions.py
@@ -17,8 +17,8 @@ g = ''
 for key in settings:
     if g != settings[key]['group']:
         g = settings[key]['group']
-        print 
-        print g
+        print
+        print g.upper()
     print u'• ' + settings[key]['title'] + ' - ' + settings[key]['help']
     with open('./actions/' + settings[key]['group'].replace(' ', '') + '-' + settings[key]['title'].replace(' ', '') + '.jsx', 'w') as f:
         f.write(s1 + key + s2);
