0565f12104e2590a140437fa8503105cda93663f
Petri Damstén <petri.damsten@gmail.com>
2018-09-26 09:59:30 +0300

    print readme text when making action scripts

--- fill_actions.py
+++ fill_actions.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+from collections import OrderedDict
 
 s1 = '''#include "../cc/main.jsx"
 init((new File($.fileName)).parent + '/../');
@@ -9,8 +10,15 @@ on'''
 s2 = 'Click();\n'
 
 with open('./ui/js/settings.json', "r") as f:
-  settings = json.loads(f.read()[15:])
+  settings = json.loads(f.read()[15:], object_pairs_hook=OrderedDict)
+
+g = ''
 
 for key in settings:
+    if g != settings[key]['group']:
+        g = settings[key]['group']
+        print 
+        print g
+    print u'• ' + settings[key]['title'] + ' - ' + settings[key]['help']
     with open('./actions/' + settings[key]['group'].replace(' ', '') + '-' + settings[key]['title'].replace(' ', '') + '.jsx', 'w') as f:
         f.write(s1 + key + s2);
