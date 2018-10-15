69f4f6d1c3272016ac5598e3b0193543dc849500
Petri Damstén <petri.damsten@gmail.com>
2018-10-15 18:43:24 +0300

    New photoshop

--- debug.sh
+++ debug.sh
@@ -14,3 +14,5 @@ EOL
 
 defaults write com.adobe.CSXS.8 PlayerDebugMode 1
 defaults write com.adobe.CSXS.8 LogLevel 6 && killall -u `whoami` cfprefsd
+defaults write com.adobe.CSXS.9 PlayerDebugMode 1
+defaults write com.adobe.CSXS.9 LogLevel 6 && killall -u `whoami` cfprefsd
