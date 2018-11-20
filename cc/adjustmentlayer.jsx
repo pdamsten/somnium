83b03897dc1b34b10eec769685121170ea54f3cd
Petri Damstén <petri.damsten@gmail.com>
2018-11-20 18:57:24 +0200

    handle curve channels

--- cc/adjustmentlayer.jsx
+++ cc/adjustmentlayer.jsx
@@ -77,6 +77,8 @@ setColorLookup = function(layer, lookup)
   }
 }
 
+var CurveTypes = {'master': 'Cmps', 'red': 'Rd  ', 'green': 'Grn ', 'blue': 'Bl  '};
+
 createCurveAdjustment = function(name, layer)
 {
   try {
@@ -102,29 +104,37 @@ createCurveAdjustment = function(name, layer)
 setCurveAdjustment = function(layer, data)
 {
   try {
-    activateLayer(layer);
-    var desc1 = new ActionDescriptor();
-    var ref1 = new ActionReference();
-    ref1.putEnumerated(cTID('AdjL'), cTID('Ordn'), cTID('Trgt'));
-    desc1.putReference(cTID('null'), ref1);
-    var desc2 = new ActionDescriptor();
-    var list1 = new ActionList();
-    var desc3 = new ActionDescriptor();
-    var ref2 = new ActionReference();
-    ref2.putEnumerated(cTID('Chnl'), cTID('Chnl'), cTID('Cmps'));
-    desc3.putReference(cTID('Chnl'), ref2);
-    var list2 = new ActionList();
-    for (var i = 0; i < data.length; ++i) {
-      var desc4 = new ActionDescriptor();
-      desc4.putDouble(cTID('Hrzn'), data[i][0]);
-      desc4.putDouble(cTID('Vrtc'), data[i][1]);
-      list2.putObject(cTID('Pnt '), desc4);
+    if (data instanceof Array) {
+      data = {'master': data};
     }
-    desc3.putList(cTID('Crv '), list2);
-    list1.putObject(cTID('CrvA'), desc3);
-    desc2.putList(cTID('Adjs'), list1);
-    desc1.putObject(cTID('T   '), cTID('Crvs'), desc2);
-    executeAction(cTID('setd'), desc1, DialogModes.NO);
+    activateLayer(layer);
+    for (var type in CurveTypes) {
+      if (type in data) {
+        var desc1 = new ActionDescriptor();
+        var ref1 = new ActionReference();
+        ref1.putEnumerated(cTID('AdjL'), cTID('Ordn'), cTID('Trgt'));
+        desc1.putReference(cTID('null'), ref1);
+        var desc2 = new ActionDescriptor();
+        desc2.putEnumerated(sTID("presetKind"), sTID("presetKindType"), sTID("presetKindCustom"));
+        var list1 = new ActionList();
+        var desc3 = new ActionDescriptor();
+        var ref2 = new ActionReference();
+        ref2.putEnumerated(cTID('Chnl'), cTID('Chnl'), cTID(CurveTypes[type]));
+        desc3.putReference(cTID('Chnl'), ref2);
+        var list2 = new ActionList();
+        for (var i = 0; i < data[type].length; ++i) {
+          var desc4 = new ActionDescriptor();
+          desc4.putDouble(cTID('Hrzn'), data[type][i][0]);
+          desc4.putDouble(cTID('Vrtc'), data[type][i][1]);
+          list2.putObject(cTID('Pnt '), desc4);
+        }
+        desc3.putList(cTID('Crv '), list2);
+        list1.putObject(cTID('CrvA'), desc3);
+        desc2.putList(cTID('Adjs'), list1);
+        desc1.putObject(cTID('T   '), cTID('Crvs'), desc2);
+        executeAction(cTID('setd'), desc1, DialogModes.NO);
+      }
+    }
     return true;
   } catch (e) {
     log(e);
