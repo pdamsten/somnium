bd05d0f3d6c4a9cb301c146964ae6089790e7e31
Petri Damstén <petri.damsten@gmail.com>
2018-12-29 10:44:59 +0200

    Don't use light group. Ensure pixels when making a selection.

--- cc/light.jsx
+++ cc/light.jsx
@@ -9,15 +9,18 @@
 onMakeCookieClick = function()
 {
   try {
+    var current = app.activeDocument.activeLayer;
     var type = settings.value('MakeCookie', 'type');
-    var group = checkGroup('Light', undefined, 'Help Layers');
+
     if (type == 0) {
       var c = [curvePoint(0, 0), curvePoint(128, -15), curvePoint(255, 0)];
-      l = createCurveAdjustment('Cookie', group);
+      l = createCurveAdjustment('Cookie');
+      l.move(current, ElementPlacement.PLACEBEFORE);
       setCurveAdjustment(l, c);
     } else {
       var c = [curvePoint(0, 0), curvePoint(128, 15), curvePoint(255, 0)];
-      l = createCurveAdjustment('Cookie', group);
+      l = createCurveAdjustment('Cookie');
+      l.move(current, ElementPlacement.PLACEBEFORE);
       setCurveAdjustment(l, c);
       invertLayerMask(l);
       setLayerBlendingMode(l, 'luminosity');
@@ -30,18 +33,19 @@ onMakeCookieClick = function()
 onMakeVignetteClick = function()
 {
   try {
+    var current = app.activeDocument.activeLayer;
     var type = settings.value('MakeVignette', 'type');
-    var group = checkGroup('Light', undefined, 'Help Layers');
     var p = 0.1;
-    var c = [app.activeDocument.height * p, app.activeDocument.width * p,
-             app.activeDocument.height - app.activeDocument.height * p,
-             app.activeDocument.width - app.activeDocument.width * p];
+    var width = app.activeDocument.width.as("px");
+    var height = app.activeDocument.height.as("px");
+    var c = [height * p, width * p, height - height * p, width - width * p];
     makeSelection('new', type, c, 0.5);
     var c = [curvePoint(0, 0), curvePoint(128, -20), curvePoint(255, 0)];
-    l = createCurveAdjustment('Vignette', group);
+    l = createCurveAdjustment('Vignette');
+    l.move(current, ElementPlacement.PLACEBEFORE);
     setCurveAdjustment(l, c);
     invertLayerMask(l);
-    setLayerMaskFeather(l, app.activeDocument.width * 0.1);
+    setLayerMaskFeather(l, width * 0.1);
     setLayerBlendingMode(l, 'luminosity');
   } catch (e) {
     log(e);
@@ -51,10 +55,11 @@ onMakeVignetteClick = function()
 onMakeWashOutClick = function()
 {
   try {
-    var group = checkGroup('Light', undefined, 'Help Layers');
+    var current = app.activeDocument.activeLayer;
     var c = [curvePoint(0, 5), curvePoint(32, 0), curvePoint(224, 0), curvePoint(255, -2.5)];
 
-    l = createCurveAdjustment('Faded / Contrast', group);
+    l = createCurveAdjustment('Faded / Contrast');
+    l.move(current, ElementPlacement.PLACEBEFORE);
     setCurveAdjustment(l, c);
     deleteLayerMask(l);
     setLayerBlendingMode(l, 'luminosity');
@@ -66,16 +71,18 @@ onMakeWashOutClick = function()
 onMakeDBClick = function()
 {
   try {
+    var current = app.activeDocument.activeLayer;
     var type = settings.value('MakeDB', 'type');
-    var group = checkGroup('Light', undefined, 'Help Layers');
 
     if (type == 1) {
-      var l = createLayer('Dodge & Burn', group);
+      var l = createLayer('Dodge & Burn');
+      l.move(current, ElementPlacement.PLACEBEFORE);
       fillLayer(l, 'gray');
       setLayerBlendingMode(l, 'overlay');
       selectTool('burn');
     } else {
-      group = createGroup('Dodge & Burn', group);
+      var group = createGroup('Dodge & Burn');
+      group.move(current, ElementPlacement.PLACEBEFORE);
       var b = [curvePoint(0, 0), curvePoint(128, -30), curvePoint(255, 0)];
       var d = [curvePoint(0, 0), curvePoint(128, 30), curvePoint(255, 0)];
       var dl = createCurveAdjustment('Dodge', group);
@@ -97,9 +104,10 @@ onMakeDBClick = function()
 onMakeHighlightClick = function()
 {
   try {
-    var group = checkGroup('Light', undefined, 'Help Layers');
-
-    var l = createSolidColorAdjustment('Hightlight', group, [255, 255, 255])
+    var current = app.activeDocument.activeLayer;
+    var l = createSolidColorAdjustment('Hightlight')
+    l.move(current, ElementPlacement.PLACEBEFORE);
+    setSolidColorAdjustment(l, [255, 255, 255]);
     setLayerBlendingMode(l, 'softLight');
     addLayerMask(l);
     invertLayerMask(l);
