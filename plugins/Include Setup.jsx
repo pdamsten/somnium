898928711b34e396140a7c772f854517433a8445
Petri Damstén <petri.damsten@gmail.com>
2018-12-29 17:11:12 +0200

    Implement setup items

--- plugins/Include Setup.jsx
+++ plugins/Include Setup.jsx
@@ -8,5 +8,17 @@
 
 onIncludeSetupClick = function()
 {
-  log('onIncludeSetupClick');
+  try {
+    var doc = app.activeDocument;
+    doc.resizeImage(UnitValue(3543, "px"), UnitValue(2365, "px"), null, ResampleMethod.BICUBICSHARPER);
+
+    var fileRef = new File("/Users/damu/Pictures/Templates/setup_items.psdt");
+    var template = app.open(fileRef);
+    var l = findLayer('Notes');
+    duplicateLayerToDoc(l, doc.name);
+    template.close(SaveOptions.DONOTSAVECHANGES);
+    app.activeDocument = doc;
+  } catch (e) {
+    log(e);
+  }
 }
