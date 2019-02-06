e3005c765055b0b5734f46f1236b1d45fb3ce5de
Petri Damstén <petri.damsten@gmail.com>
2019-02-06 16:04:44 +0200

    new api

--- cc/light.jsx
+++ cc/light.jsx
@@ -9,21 +9,23 @@
 onMakeCookieClick = function()
 {
   try {
-    var current = app.activeDocument.activeLayer;
+    var doc = app.activeDocument;
+    var current = doc.activeLayer;
     var type = settings.value('MakeCookie', 'type');
 
     if (type == 0) {
       var c = [curvePoint(0, 0), curvePoint(128, -15), curvePoint(255, 0)];
-      l = createCurveAdjustment('Cookie');
+      l = doc.addCurveAdjustment('Cookie');
       l.move(current, ElementPlacement.PLACEBEFORE);
-      setCurveAdjustment(l, c);
+      l.setAdjustment(c);
+      l.setBlendingMode('luminosity');
     } else {
       var c = [curvePoint(0, 0), curvePoint(128, 15), curvePoint(255, 0)];
-      l = createCurveAdjustment('Cookie');
+      l = doc.addCurveAdjustment('Cookie');
       l.move(current, ElementPlacement.PLACEBEFORE);
-      setCurveAdjustment(l, c);
-      invertLayerMask(l);
-      setLayerBlendingMode(l, 'luminosity');
+      l.setAdjustment(c);
+      l.invertMask();
+      l.setBlendingMode('luminosity');
     }
   } catch (e) {
     log(e);
@@ -33,20 +35,21 @@ onMakeCookieClick = function()
 onMakeVignetteClick = function()
 {
   try {
-    var current = app.activeDocument.activeLayer;
+    var doc = app.activeDocument;
+    var current = doc.activeLayer;
     var type = settings.value('MakeVignette', 'type');
     var p = 0.1;
     var width = app.activeDocument.width.as("px");
     var height = app.activeDocument.height.as("px");
     var c = [height * p, width * p, height - height * p, width - width * p];
-    makeSelection('new', type, c, 0.5);
+    doc.makeSelection('new', type, c, 0.5);
     var c = [curvePoint(0, 0), curvePoint(128, -20), curvePoint(255, 0)];
-    l = createCurveAdjustment('Vignette');
+    l = doc.addCurveAdjustment('Vignette');
     l.move(current, ElementPlacement.PLACEBEFORE);
-    setCurveAdjustment(l, c);
-    invertLayerMask(l);
-    setLayerMaskFeather(l, width * 0.1);
-    setLayerBlendingMode(l, 'luminosity');
+    l.setAdjustment(c);
+    l.invertMask();
+    l.setMaskFeather(width * 0.1);
+    l.setBlendingMode('luminosity');
   } catch (e) {
     log(e);
   }
@@ -55,14 +58,15 @@ onMakeVignetteClick = function()
 onMakeWashOutClick = function()
 {
   try {
-    var current = app.activeDocument.activeLayer;
+    var doc = app.activeDocument;
+    var current = doc.activeLayer;
     var c = [curvePoint(0, 5), curvePoint(32, 0), curvePoint(224, 0), curvePoint(255, -2.5)];
 
-    l = createCurveAdjustment('Faded / Contrast');
+    l = doc.addCurveAdjustment('Faded / Contrast');
     l.move(current, ElementPlacement.PLACEBEFORE);
-    setCurveAdjustment(l, c);
-    deleteLayerMask(l);
-    setLayerBlendingMode(l, 'luminosity');
+    l.setCurveAdjustment(c);
+    l.deleteMask();
+    l.setBlendingMode('luminosity');
   } catch (e) {
     log(e);
   }
@@ -71,31 +75,32 @@ onMakeWashOutClick = function()
 onMakeDBClick = function()
 {
   try {
-    var current = app.activeDocument.activeLayer;
+    var doc = app.activeDocument;
+    var current = doc.activeLayer;
     var type = settings.value('MakeDB', 'type');
 
     if (type == 1) {
-      var l = createLayer('Dodge & Burn');
+      var l = doc.addLayer('Dodge & Burn');
       l.move(current, ElementPlacement.PLACEBEFORE);
-      fillLayer(l, 'gray');
-      setLayerBlendingMode(l, 'overlay');
-      selectTool('burn');
+      l.fill('gray');
+      l.setBlendingMode('overlay');
+      Photoshop.selectTool('burn');
     } else {
-      var group = createGroup('Dodge & Burn');
+      var group = doc.addGroup('Dodge & Burn');
       group.move(current, ElementPlacement.PLACEBEFORE);
       var b = [curvePoint(0, 0), curvePoint(128, -30), curvePoint(255, 0)];
       var d = [curvePoint(0, 0), curvePoint(128, 30), curvePoint(255, 0)];
-      var dl = createCurveAdjustment('Dodge', group);
-      var bl = createCurveAdjustment('Burn', group);
-      setCurveAdjustment(dl, d);
-      setCurveAdjustment(bl, b);
-      invertLayerMask(dl);
-      invertLayerMask(bl);
-      setLayerBlendingMode(dl, 'luminosity');
-      setLayerBlendingMode(bl, 'luminosity');
-      setDefaultColors();
-      app.activeDocument.activeLayer = group;
-      selectTool('brush'); // TODO brush preset?
+      var dl = doc.addCurveAdjustment('Dodge', group);
+      var bl = doc.addCurveAdjustment('Burn', group);
+      dl.setAdjustment(d);
+      bl.setAdjustment(b);
+      dl.invertMask();
+      bl.invertMask();
+      dl.setBlendingMode('luminosity');
+      bl.setBlendingMode('luminosity');
+      Photoshop.setDefaultColors();
+      group.activate();;
+      Photoshop.selectTool('brush'); // TODO brush preset?
     }
   } catch (e) {
     log(e);
@@ -105,13 +110,14 @@ onMakeDBClick = function()
 onMakeHighlightClick = function()
 {
   try {
-    var current = app.activeDocument.activeLayer;
-    var l = createSolidColorAdjustment('Hightlight')
+    var doc = app.activeDocument;
+    var current = doc.activeLayer;
+    var l = doc.addSolidColorAdjustment('Hightlight')
     l.move(current, ElementPlacement.PLACEBEFORE);
-    setSolidColorAdjustment(l, [255, 255, 255]);
-    setLayerBlendingMode(l, 'softLight');
-    addLayerMask(l);
-    invertLayerMask(l);
+    l.setAdjustment([255, 255, 255]);
+    l.setBlendingMode('soft light');
+    l.addMask();
+    l.invertMask(l);
   } catch (e) {
     log(e);
   }
