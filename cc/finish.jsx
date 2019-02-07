bfaec88482f4b5dabb6fc9e890fa0a3076595822
Petri Damstén <petri.damsten@gmail.com>
2019-02-07 10:44:27 +0200

    save new smart obj

--- cc/finish.jsx
+++ cc/finish.jsx
@@ -12,7 +12,7 @@ onHighPassSharpeningClick = function()
     var doc = app.activeDocument;
     var current = doc.activeLayer;
     var layer = doc.stampCurrentAndBelow('High Pass Sharpening');
-    layer.convertToSmartObject();
+    layer = layer.convertToSmartObject();
     layer.move(current, ElementPlacement.PLACEBEFORE);
     layer.applyHighPassEx(2.6, false);
     layer.setBlendingMode('hard light');
@@ -27,7 +27,7 @@ onAddNoiseClick = function()
     var doc = app.activeDocument;
     var current = doc.activeLayer;
     var layer = doc.addSolidColorAdjustment('Noise', [128, 128, 128]);
-    layer.convertToSmartObject();
+    layer = layer.convertToSmartObject();
     layer.deleteMask();
     layer.move(current, ElementPlacement.PLACEBEFORE);
     layer.applyAddNoiseEx(2.0, 'gaussian', true, false);
