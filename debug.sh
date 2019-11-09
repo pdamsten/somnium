cf61c4058710b5a676195be4489413443152a233
Petri Damstén <petri.damsten@gmail.com>
2019-11-09 18:19:21 +0200

    add 10 (unnecessary?)

--- debug.sh
+++ debug.sh
@@ -24,3 +24,5 @@ defaults write com.adobe.CSXS.8 PlayerDebugMode 1
 defaults write com.adobe.CSXS.8 LogLevel 6 && killall -u `whoami` cfprefsd
 defaults write com.adobe.CSXS.9 PlayerDebugMode 1
 defaults write com.adobe.CSXS.9 LogLevel 6 && killall -u `whoami` cfprefsd
+defaults write com.adobe.CSXS.10 PlayerDebugMode 1
+defaults write com.adobe.CSXS.10 LogLevel 6 && killall -u `whoami` cfprefsd
