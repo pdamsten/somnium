b904dd3ec66b241c12b73e168eab3cf49cd5963e
Petri Damstén <petri.damsten@gmail.com>
2020-06-05 14:47:01 +0300

    mv 2 gpl license

--- cc/helpers.jsx
+++ cc/helpers.jsx
@@ -1,10 +1,23 @@
-//**************************************************************************
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-//   Copyright (c) 2018 by Petri Damstén <petri.damsten@gmail.com>
+//  Copyright (c) 2018-2020 by Petri Damstén <petri.damsten@gmail.com>
 //
-//   Helpers tab code
+//  This file is part of Somnium.
 //
-//**************************************************************************
+//  Somnium is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Somnium.  If not, see <https://www.gnu.org/licenses/>.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const helpGroupName = 'Help Layers';
 const grey50 = [128, 128, 128];
