80aca3114d294922815c5fa64d47a85cb6f70f16
Petri Damstén <petri.damsten@gmail.com>
2022-01-29 09:18:24 +0200

    save switch case

--- plugins/macro_keyboard.jsx
+++ plugins/macro_keyboard.jsx
@@ -1,12 +1,22 @@
 #include "../cc/main.jsx"
 init((new File($.fileName)).parent + '/../');
 
-var reply = "";
+var key = NaN;
 var conn = new Socket;
 
 if (conn.open("127.0.0.1:7563")) {
     conn.write("GET\n");
-    reply = conn.read(999999);
-    alert(reply);
+    key = parseInt(conn.read(999999));
     conn.close();
+}
+
+switch(key) {
+  case 0:
+    alert('First key');
+    break;
+  case isNaN(key):
+    alert('Invalid key from the macro keyboard.');
+    break;
+  default:
+    alert('No handler for this key: ' + key);
 }
