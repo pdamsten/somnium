3dcb16593d1cfe44cb6f5bf94a94f9068b5b0249
Petri Damstén <petri.damsten@gmail.com>
2022-02-02 17:27:03 +0200

    new layout

--- plugins/macro_keyboard.jsx
+++ plugins/macro_keyboard.jsx
@@ -29,51 +29,53 @@ switch(key) {
   case 20: onMatchTonesClick(); break;
   case 21: onMakeLightnessClick(); break;
   case 23: Photoshop.zoom(20); break;
+  case 47: Photoshop.zoom(200); break;
   // Selection
-  case 32: Photoshop.selectTool('cropTool'); break;
-  case 33: app.activeDocument.inverseSelection(); break;
-  case 35: Photoshop.selectTool('marqueeRectTool'); break;
-  case 45: Photoshop.selectTool('polySelTool'); break;
-  case 46: Photoshop.selectTool('lassoTool'); break;
-  case 47: Photoshop.selectTool('magicLassoTool'); break;
+  case 31: Photoshop.selectTool('cropTool'); break;
+  case 32: app.activeDocument.inverseSelection(); break;
+  case 34: Photoshop.selectTool('marqueeRectTool'); break;
+  case 43: app.activeDocument.clearSelection(); break;
+  case 44: Photoshop.selectTool('polySelTool'); break;
+  case 45: Photoshop.selectTool('lassoTool'); break;
+  case 46: Photoshop.selectTool('magicLassoTool'); break;
   // Undo & Redo
   case 57: Photoshop.undo(); break;
   case 59: Photoshop.redo(); break;
   // Arrow Group
   case 58: Photoshop.selectTool('moveTool'); break;
-  case 71: Photoshop.zoom(200); break;
   // Basic Tools
-  case 30: Photoshop.selectTool('curvaturePenTool'); break;
-  case 42: Photoshop.selectTool('magicStampTool'); break;
-  case 54: Photoshop.selectTool('cloneStampTool'); break;
-  case 66: Photoshop.selectTool('spotHealingBrushTool'); break;
-  case 78: Photoshop.selectTool('gradientTool'); break;
-  case 31: Photoshop.selectTool('typeCreateOrEditTool'); break;
-  case 43: Photoshop.selectToolPreset('paintbrushTool', 'Hardness 95%'); break;
-  case 55: Photoshop.selectToolPreset('paintbrushTool', 'Flow 100%'); break;
+  case 29: Photoshop.selectTool('curvaturePenTool'); break;
+  case 41: Photoshop.selectTool('magicStampTool'); break;
+  case 53: Photoshop.selectTool('cloneStampTool'); break;
+  case 65: Photoshop.selectTool('spotHealingBrushTool'); break;
+  case 77: Photoshop.selectTool('gradientTool'); break;
+  case 30: Photoshop.selectTool('typeCreateOrEditTool'); break;
+  case 66: Photoshop.selectToolPreset('paintbrushTool', 'Hardness 95%'); break;
+  case 78: Photoshop.selectToolPreset('paintbrushTool', 'Flow 100%'); break;
   case 67: Photoshop.selectToolPreset('paintbrushTool', 'Flow 10%'); break;
   case 79: Photoshop.selectToolPreset('paintbrushTool', 'Flow 3%'); break;
-  case 56: onMakeCleaningClick(); break;
-  case 68: Photoshop.swapColors(); break;
-  case 80: Photoshop.setDefaultColors(); break;
+  case 54: onMakeCleaningClick(); break;
+  case 55: app.activeDocument.activeLayer.fill('foregroundColor'); break;
+  case 56: Photoshop.swapColors(); break;
+  case 68: Photoshop.setDefaultColors(); break;
   // Advanced Tools
-  case 28: app.activeDocument.activeLayer.contractMask(); break;
-  case 40: onMakeFSClick(); break;
-  case 52: onCombineDocumentsClick(); break;
-  case 64: onUpdateFromUnderClick(); break;
-  case 76: onStampUnderClick(); break;
-  case 29: app.activeDocument.activeLayer.expandMask(); break;
-  case 41: onMakeMatchClick(); break;
-  case 54: onFillEmptyClick(); break;
-  case 65: onSmartKeepingMaskClick(); break;
-  case 77: onSmartFromUnderClick(); break;
+  case 26: app.activeDocument.activeLayer.contractMask(); break;
+  case 38: onMakeFSClick(); break;
+  case 50: onCombineDocumentsClick(); break;
+  case 62: onUpdateFromUnderClick(); break;
+  case 74: onStampUnderClick(); break;
+  case 27: app.activeDocument.activeLayer.expandMask(); break;
+  case 39: onMakeMatchClick(); break;
+  case 52: onFillEmptyClick(); break;
+  case 63: onSmartKeepingMaskClick(); break;
+  case 75: onSmartFromUnderClick(); break;
   // Finishing
-  case 50: onHighPassSharpeningClick(); break;
-  case 62: onMakeWashOutClick(); break;
-  case 74: onMakeVignetteClick(); break;
-  case 51: onAddNoiseClick(); break;
-  case 63: onMakeDBClick(); break;
-  case 75: onMakeCookieClick(); break;
+  case 48: onHighPassSharpeningClick(); break;
+  case 60: onMakeWashOutClick(); break;
+  case 72: onMakeVignetteClick(); break;
+  case 49: onAddNoiseClick(); break;
+  case 61: onMakeDBClick(); break;
+  case 73: onMakeCookieClick(); break;
 
   //case : (); break;
   case isNaN(key):
