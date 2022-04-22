c71bf0b5d285a93b85b8dbe195fc984709ae7304
Petri Damstén <petri.damsten@gmail.com>
2022-04-22 20:33:00 +0300

    Start of UXP version of plugin

--- make.bat
+++
@@ -1,2 +0,0 @@
-ZXPSignCmd.exe -selfSignedCert FI PS PetriDamsten "Petri Damsten" "password" Z:\tmp\selfDB.p12
-ZXPSignCmd.exe -sign Z:\tmp\com.petridamsten.somnium Z:\tmp\com.petridamsten.somnium.zxp Z:\tmp\selfDB.p12 "password" -tsa http://timestamp.digicert.com/
