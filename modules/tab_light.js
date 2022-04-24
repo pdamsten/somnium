2f8c6a0f8b00d6d5fada242b55153283ea1ebde2
Petri Damstén <petri.damsten@gmail.com>
2022-04-24 08:04:08 +0300

    start for light tab

---
+++ modules/tab_light.js
@@ -0,0 +1,43 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//  Copyright (c) 2022 by Petri Damstén <petri.damsten@gmail.com>
+//
+//  This file is part of Somnium.
+//
+//  Somnium is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Somnium.  If not, see <https://www.gnu.org/licenses/>.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+module.exports = {
+  makeVignette
+}
+
+async function makeVignette()
+{
+  var vignetteDlg = {
+    'title': 'Vignette',
+    'width': 350,
+    'height': 5000,
+    "items": {
+      "type": {
+        "title": "Style:",
+        "type": "selection",
+        "value": 0,
+        "values": ["Elliptical", "Rectangular"]
+      }
+    }
+  };
+  var res = await jdialog.open(vignetteDlg);
+  console.log(res);
+}
