c97d75311d279aaec775190a108cffe5832b3a89
Petri Damstén <petri.damsten@gmail.com>
2022-04-24 08:04:54 +0300

    2 jdialog

--- ui/js/common.js
+++ ui/js/common.js
@@ -19,69 +19,9 @@
 //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function input(id, cls, type, val)
-{
-  cls = (cls == undefined) ? '': ' ' + cls;
-  type = (type == undefined) ? '': ' data-type="' + type + '" ';
-  val = (val == undefined) ? '': val;
-  return '<input id="' + id + '" class="ccwidget ' + cls + '" value="' + val + '"' + type + '>';
-}
-
 (function () {
   'use strict';
 
-  // Handle all clickable elements
-  $(".jsonWidgets").on("click", ".clickable", function (e) {
-    var type = $(this).data("type");
-    var fn;
-    var id;
-    var params = [];
 
-    if (type == "folderBrowse") {
-      fn = 'BrowseFolder';
-      params.push('"' + $(this).data("title") + '"');
-      id = $(this).data("id");
-      params.push('"' + $('#' + id).val() + '"');
-    } else if (type == "color") {
-      fn = 'ColorPicker';
-      id = $(this).attr('id');
-      params.push('"' + $(this).css('background-color') + '"');
-    }
-
-    fn = 'on' + fn + 'Click(' + params.join(',') + ')';
-    console.log(fn);
-    /*
-    csInterface.evalScript(fn, function(result) {
-      console.log(result);
-      if (result != 'null') {
-        var a = id.split('-');
-        if (type == "folderBrowse") {
-          $('#' + id).val(result);
-        } else if (type == "color") {
-          $('#' + id).css('background-color', result)
-        }
-        var fn = 'settings.value("' + a[0] + '","' + a[1] + '","' + result + '");'
-        csInterface.evalScript(fn);
-      }
-    });
-    */
-    e.stopPropagation();
-  });
-
-  $(".jsonWidgets").on('change', "input, select", function(e) {
-    var a = $(this).attr('id').split('-');
-    if ($(this).data('type') == 'pixelsize') {
-      a[1] = a[1].slice(0, -2);
-      var v = '"' + $('#' + a[0] + '-' + a[1] + '_x').val() + 'x' + $('#' + a[0] +
-              '-' + a[1] + '_y').val() + '"';
-    } else if ($(this).data('type') == 'boolean') {
-      var v = $(this).prop('checked');
-    } else {
-      var v = '"' + this.value + '"';
-    }
-    var fn = 'settings.value("' + a[0] + '","' + a[1] + '",' + v + ');';
-    //console.log(fn);
-    //csInterface.evalScript(fn);
-  });
 
 }());
